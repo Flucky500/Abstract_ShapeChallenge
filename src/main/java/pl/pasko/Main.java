e15edea89409ccca99ebc2296d7f126ef9541c61
Lukasz Pasko <flucky500@gmail.com>
2022-03-10 22:47:11 +0100

    Create classes Shape, Rectangle, Square, Circle, Triangle

--- src/main/java/pl/pasko/Main.java
+++ src/main/java/pl/pasko/Main.java
@@ -5,23 +5,23 @@ public class Main {
     public static void main(String[] args) {
 
         Rectangle rectangle = new Rectangle(2,4);
-        System.out.println("Area = " + rectangle.getArea());
-        System.out.println("Perimeter = " + rectangle.getPerimeter());
+        System.out.println("Rectangle area = " + rectangle.getArea());
+        System.out.println("Retangle perimeter = " + rectangle.getPerimeter());
         System.out.println("******************");
 
         Square square = new Square(2);
-        System.out.println("Area = " + square.getArea());
-        System.out.println("Perimeter = " + square.getPerimeter());
+        System.out.println("Square area = " + square.getArea());
+        System.out.println("Square perimeter = " + square.getPerimeter());
         System.out.println("******************");
 
         Circle circle = new Circle(5);
-        System.out.println("Area = " + circle.getArea());
-        System.out.println("Perimeter = " + circle.getPerimeter());
+        System.out.println("Circle area = " + circle.getArea());
+        System.out.println("Circle perimeter = " + circle.getPerimeter());
         System.out.println("******************");
 
-        Triangle triangle = new Triangle(2,2,2,1);
-        System.out.println("Area = " + triangle.getArea());
-        System.out.println("Perimeter = " + triangle.getPerimeter());
+        Triangle triangle = new Triangle(2,2,2);
+        System.out.println("Triangle area = " + triangle.getArea());
+        System.out.println("Triangle perimeter = " + triangle.getPerimeter());
         System.out.println("******************");
 
 
